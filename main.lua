9f0e65448c0d62c5c0727e1c7c003d39929266f3
Simon Kers <skers@kth.se>
2016-04-05 07:30:05 +0200

    Updating description with player controls

--- main.lua
+++ main.lua
@@ -6,7 +6,13 @@ DESCRIPTION = {
   "    2. Any live cell with two or three live neighbours lives on to the next generation.",
   "    3. Any live cell with more than three live neighbours dies, as if by overcrowding.",
   "    4. Any dead cell with exactly three live neighbours becomes a live cell, as if by reproduction.",
-  "        "
+  "        ",
+  "        ",
+  "  Controls:",
+  "        ",
+  "    Use right and left arrows to browse through life patterns.",
+  "        ",
+  "        ",
 }
 
 MAXHEALTH = 3
